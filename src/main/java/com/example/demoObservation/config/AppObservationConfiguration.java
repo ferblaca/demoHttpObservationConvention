60d75ef271a7d2f8e57cd0d506df4e8bd0000c04
ferblaca <fernandobc@ext.inditex.com>
2023-04-06 16:26:19 +0200

    add api client with openapi-generator

--- src/main/java/com/example/demoObservation/config/AppObservationConfiguration.java
+++ src/main/java/com/example/demoObservation/config/AppObservationConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.demoObservation.config;
 
+import demo.ApiClient;
+import demo.openapi.PetApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,27 +11,36 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppObservationConfiguration {
 
-  //  private static final KeyValue URI_NONE = KeyValue.of(ClientHttpObservationDocumentation.LowCardinalityKeyNames.URI, "none");
+    //  private static final KeyValue URI_NONE = KeyValue.of(ClientHttpObservationDocumentation.LowCardinalityKeyNames.URI, "none");
 
-  //    @Bean
-  //  public ObservationConvention customObservationConvention() {
-  //    return new CustomObservationConvention();
-  //  }
+    //    @Bean
+    //  public ObservationConvention customObservationConvention() {
+    //    return new CustomObservationConvention();
+    //  }
 
-  //    @Bean
-  //  ObservationPredicate disableHttpServerObservationsFromName() {
-  //    return (name, context) -> !name.startsWith("http.server.");
-  //  }
+    //    @Bean
+    //  ObservationPredicate disableHttpServerObservationsFromName() {
+    //    return (name, context) -> !name.startsWith("http.server.");
+    //  }
 
-  //    @Bean
-  //  ObservationPredicate disableHttpClientObservationsWithoutUri() {
-  //    return (name, context) ->
-  //        name.startsWith("http.client.requests") && context.getAllKeyValues().stream().anyMatch(keyValue -> equals(URI_NONE));
-  //  }
+    //    @Bean
+    //  ObservationPredicate disableHttpClientObservationsWithoutUri() {
+    //    return (name, context) ->
+    //        name.startsWith("http.client.requests") && context.getAllKeyValues().stream().anyMatch(keyValue -> equals(URI_NONE));
+    //  }
 
-  @Bean
-  public RestTemplate restClient(RestTemplateBuilder builder) {
-    return builder.build();
-  }
+    @Bean
+    public RestTemplate restClient(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public PetApi petApi(RestTemplate restClient,
+                         @Value("${server.port}") String port) {
+        ApiClient apiClient = new ApiClient(restClient);
+        apiClient.setBasePath("http://localhost:" + port + "/app-rest");
+        PetApi petApi = new PetApi(apiClient);
+        return petApi;
+    }
 
 }
--- src/main/java/com/example/demoObservation/controller/AppController.java
+++ src/main/java/com/example/demoObservation/controller/AppController.java
@@ -1,5 +1,7 @@
 package com.example.demoObservation.controller;
 
+import demo.openapi.PetApi;
+import demo.openapi.model.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +12,8 @@ public class AppController {
 
     @Autowired
     private RestTemplate restClient;
+    @Autowired
+    private PetApi petApi;
 
     @GetMapping(path = "/invoke")
     public String invokeRestClient() {
@@ -29,6 +33,18 @@ public class AppController {
     @RequestMapping(value = {"/client-invoked-with-template/{param}"}, method = RequestMethod.GET)
     public String invokedByClient(@PathVariable String param) {
         return "hello! " + param;
+    }
+
+    @GetMapping(path = "/invoke-api-client-templatized")
+    public Pet invokeRestApiClientTemplatized() {
+        return petApi.getPet2ById(2L);
+    }
+
+    @RequestMapping(value = {"/pet2/{petId}"}, method = RequestMethod.GET)
+    public Pet pet(@PathVariable Long petId) {
+        Pet pet = new Pet();
+        pet.setId(petId);
+        return pet;
     }
 
 }
